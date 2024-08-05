afbb1a9f6cf1fb843d19c8c78928121f9d5c0bf2
karimanhossam <karimanhossam67@gmail.com>
2024-08-05 10:42:34 +0400

    fix: production env detection

--- clients/webpack.config.js
+++ clients/webpack.config.js
@@ -4,100 +4,101 @@ const path = require('path');
 const Dotenv = require('dotenv-webpack');
 
 const deps = require("./package.json").dependencies;
-
 const printCompilationMessage = require('./compilation.config.js');
-const isProduction = process.env.NODE_ENV === 'production';
 
-module.exports = (_, argv) => ({
-  output: {
-    publicPath: isProduction ? process.env.REACT_APP_CLIENTS_URL : "http://localhost:3001/",
+module.exports = (_, argv) => {
+  const isProduction = argv.mode === 'production';
 
-  },
+  return {
+    output: {
+      publicPath: isProduction ? process.env.REACT_APP_CLIENTS_URL : "http://localhost:3001/",
+    },
 
-  resolve: {
-    extensions: [".tsx", ".ts", ".jsx", ".js", ".json"],
-  },
+    resolve: {
+      extensions: [".tsx", ".ts", ".jsx", ".js", ".json"],
+    },
 
-  devServer: {
-    port: 3001,
-    historyApiFallback: true,
-    watchFiles: [path.resolve(__dirname, 'src')],
-    onListening: function (devServer) {
-      const port = devServer.server.address().port
+    devServer: {
+      port: 3001,
+      historyApiFallback: true,
+      watchFiles: [path.resolve(__dirname, 'src')],
+      onListening: function (devServer) {
+        const port = devServer.server.address().port;
 
-      printCompilationMessage('compiling', port)
+        printCompilationMessage('compiling', port);
 
-      devServer.compiler.hooks.done.tap('OutputMessagePlugin', (stats) => {
-        setImmediate(() => {
-          if (stats.hasErrors()) {
-            printCompilationMessage('failure', port)
-          } else {
-            printCompilationMessage('success', port)
-          }
-        })
-      })
-    }
-  },
+        devServer.compiler.hooks.done.tap('OutputMessagePlugin', (stats) => {
+          setImmediate(() => {
+            if (stats.hasErrors()) {
+              printCompilationMessage('failure', port);
+            } else {
+              printCompilationMessage('success', port);
+            }
+          });
+        });
+      }
+    },
 
-  module: {
-    rules: [
-      {
-        test: /\.m?js/,
-        type: "javascript/auto",
-        resolve: {
-          fullySpecified: false,
+    module: {
+      rules: [
+        {
+          test: /\.m?js/,
+          type: "javascript/auto",
+          resolve: {
+            fullySpecified: false,
+          },
         },
-      },
-      {
-        test: /\.(css|s[ac]ss)$/i,
-        use: ["style-loader", "css-loader", "postcss-loader"],
-      },
-      {
-        test: /\.(ts|tsx|js|jsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
+        {
+          test: /\.(css|s[ac]ss)$/i,
+          use: ["style-loader", "css-loader", "postcss-loader"],
         },
-      },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: "asset/resource",
-      },
-      {
-        test: /\.(woff|woff2|eot|ttf|otf)$/i,
-        type: "asset/resource",
-      },
-      { test: /\.json$/, type: "json" },
+        {
+          test: /\.(ts|tsx|js|jsx)$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader",
+          },
+        },
+        {
+          test: /\.(png|svg|jpg|jpeg|gif)$/i,
+          type: "asset/resource",
+        },
+        {
+          test: /\.(woff|woff2|eot|ttf|otf)$/i,
+          type: "asset/resource",
+        },
+        { test: /\.json$/, type: "json" },
+      ],
+    },
+
+    plugins: [
+      new ModuleFederationPlugin({
+        name: "clients",
+        filename: "remoteEntry.js",
+        remotes: {
+          host: `host@${isProduction ? process.env.REACT_APP_HOST_URL : "http://localhost:3000"}/remoteEntry.js`,
+          library: `library@${isProduction ? process.env.REACT_APP_LIBRARY_URL : "http://localhost:3002"}/remoteEntry.js`,
+        },
+        exposes: {
+          "./App": "./src/App.jsx",
+          './hooks/useSwitchClientsLanguage': './src/hooks/useSwitchLanguage',
+        },
+        shared: {
+          ...deps,
+          react: {
+            singleton: true,
+            requiredVersion: deps.react,
+          },
+          "react-dom": {
+            singleton: true,
+            requiredVersion: deps["react-dom"],
+          },
+        },
+      }),
+      new HtmlWebPackPlugin({
+        template: "./src/index.html",
+      }),
+      new Dotenv()
     ],
-  },
-
-  plugins: [
-    new ModuleFederationPlugin({
-      name: "clients",
-      filename: "remoteEntry.js",
-      remotes: {
-        host: `host@${isProduction ? process.env.REACT_APP_HOST_URL : "http://localhost:3000"}/remoteEntry.js`,
-        library:`library@${ isProduction ? process.env.REACT_APP_LIBRARY_URL : "http://localhost:3002"}/remoteEntry.js`,
-      },
-      exposes: {
-        "./App": "./src/App.jsx",
-        './hooks/useSwitchClientsLanguage': './src/hooks/useSwitchLanguage',
-      },
-      shared: {
-        ...deps,
-        react: {
-          singleton: true,
-          requiredVersion: deps.react,
-        },
-        "react-dom": {
-          singleton: true,
-          requiredVersion: deps["react-dom"],
-        },
-      },
-    }),
-    new HtmlWebPackPlugin({
-      template: "./src/index.html",
-    }),
-    new Dotenv()
-  ],
-});
+  };
+};
--- host/webpack.config.js
+++ host/webpack.config.js
@@ -4,101 +4,102 @@ const path = require("path");
 const Dotenv = require("dotenv-webpack");
 
 const deps = require("./package.json").dependencies;
-
 const printCompilationMessage = require("./compilation.config.js");
 
-const isProduction = process.env.NODE_ENV === 'production';
+module.exports = (_, argv) => {
+  const isProduction = argv.mode === 'production';
 
-module.exports = (_, argv) => ({
-  output: {
-    publicPath: isProduction ? process.env.REACT_APP_HOST_URL : "http://localhost:3000/",
-  },
-  resolve: {
-    extensions: [".tsx", ".ts", ".jsx", ".js", ".json"],
-  },
-
-  devServer: {
-    port: 3000,
-    historyApiFallback: true,
-    watchFiles: [path.resolve(__dirname, "src")],
-    onListening: function (devServer) {
-      const port = devServer.server.address().port;
-
-      printCompilationMessage("compiling", port);
-
-      devServer.compiler.hooks.done.tap("OutputMessagePlugin", (stats) => {
-        setImmediate(() => {
-          if (stats.hasErrors()) {
-            printCompilationMessage("failure", port);
-          } else {
-            printCompilationMessage("success", port);
-          }
-        });
-      });
+  return {
+    output: {
+      publicPath: isProduction ? process.env.REACT_APP_HOST_URL : "http://localhost:3000/",
     },
-  },
 
-  module: {
-    rules: [
-      {
-        test: /\.m?js/,
-        type: "javascript/auto",
-        resolve: {
-          fullySpecified: false,
+    resolve: {
+      extensions: [".tsx", ".ts", ".jsx", ".js", ".json"],
+    },
+
+    devServer: {
+      port: 3000,
+      historyApiFallback: true,
+      watchFiles: [path.resolve(__dirname, "src")],
+      onListening: function (devServer) {
+        const port = devServer.server.address().port;
+
+        printCompilationMessage("compiling", port);
+
+        devServer.compiler.hooks.done.tap("OutputMessagePlugin", (stats) => {
+          setImmediate(() => {
+            if (stats.hasErrors()) {
+              printCompilationMessage("failure", port);
+            } else {
+              printCompilationMessage("success", port);
+            }
+          });
+        });
+      },
+    },
+
+    module: {
+      rules: [
+        {
+          test: /\.m?js/,
+          type: "javascript/auto",
+          resolve: {
+            fullySpecified: false,
+          },
         },
-      },
-      {
-        test: /\.(css|s[ac]ss)$/i,
-        use: ["style-loader", "css-loader", "postcss-loader"],
-      },
-      {
-        test: /\.(ts|tsx|js|jsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
+        {
+          test: /\.(css|s[ac]ss)$/i,
+          use: ["style-loader", "css-loader", "postcss-loader"],
         },
-      },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: "asset/resource",
-      },
-      {
-        test: /\.(woff|woff2|eot|ttf|otf)$/i,
-        type: "asset/resource",
-      },
-      { test: /\.json$/, type: "json" },
+        {
+          test: /\.(ts|tsx|js|jsx)$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader",
+          },
+        },
+        {
+          test: /\.(png|svg|jpg|jpeg|gif)$/i,
+          type: "asset/resource",
+        },
+        {
+          test: /\.(woff|woff2|eot|ttf|otf)$/i,
+          type: "asset/resource",
+        },
+        { test: /\.json$/, type: "json" },
+      ],
+    },
+
+    plugins: [
+      new ModuleFederationPlugin({
+        name: "host",
+        filename: "remoteEntry.js",
+        remotes: {
+          clients: `clients@${isProduction ? process.env.REACT_APP_CLIENTS_URL : "http://localhost:3001"}/remoteEntry.js`,
+          library: `library@${isProduction ? process.env.REACT_APP_LIBRARY_URL : "http://localhost:3002"}/remoteEntry.js`,
+        },
+        exposes: {
+          "./Error": "./src/pages/Error.jsx",
+          "./hooks/useLanguageDirection": "./src/hooks/useLanguageDirection.js",
+          "./hooks/useAuthMiddleware": "./src/hooks/useAuthMiddleware.js",
+        },
+        shared: {
+          ...deps,
+          react: {
+            singleton: true,
+            requiredVersion: deps.react,
+          },
+          "react-dom": {
+            singleton: true,
+            requiredVersion: deps["react-dom"],
+          },
+        },
+      }),
+      new HtmlWebPackPlugin({
+        template: "./src/index.html",
+      }),
+      new Dotenv(),
     ],
-  },
-
-  plugins: [
-    new ModuleFederationPlugin({
-      name: "host",
-      filename: "remoteEntry.js",
-      remotes: {
-        clients: `clients@${ isProduction ? process.env.REACT_APP_CLIENTS_URL : "http://localhost:3001"}/remoteEntry.js`,
-        library: `library@${ isProduction ? process.env.REACT_APP_LIBRARY_URL : "http://localhost:3002"}/remoteEntry.js`,
-      },
-
-      exposes: {
-        "./Error": "./src/pages/Error.jsx",
-        "./hooks/useLanguageDirection": "./src/hooks/useLanguageDirection.js",
-        "./hooks/useAuthMiddleware": "./src/hooks/useAuthMiddleware.js",
-      },
-      shared: {
-        ...deps,
-        react: {
-          singleton: true,
-          requiredVersion: deps.react,
-        },
-        "react-dom": {
-          singleton: true,
-          requiredVersion: deps["react-dom"],
-        },
-      },
-    }),
-    new HtmlWebPackPlugin({
-      template: "./src/index.html",
-    }),
-    new Dotenv(),
-  ],
-});
+  };
+};
